ad06d2c9f656a74fa0caf292977313f97e779733
Bruno Ferreira <brunoo.lcf10@gmail.com>
2023-06-13 16:53:05 -0300

    Adicionando primeiros arquivos em React

---
+++ blogpessoal/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="images/dev.jpg" alt="Imagem de fundo de linhas de código" className="img"/>
+        </>
+    );
+}
+
+export default Home;
