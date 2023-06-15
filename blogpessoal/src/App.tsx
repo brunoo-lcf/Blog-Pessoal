1887c864ae3b177a7402b87bbc1f3db00cc02ae1
Bruno Ferreira <brunoo.lcf10@gmail.com>
2023-06-14 22:27:41 -0300

    Adicionando customização de NavBar, Home e Footer

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/home/Home'
+import React from 'react';
+import './App.css';
+import Navbar from './components/static/navBar/Navbar.tsx';
+import Footer from './components/static/footer/Footer.tsx'
+import Home from './pages/home/Home.tsx';
 
 function App() {
   return(
+    <>
+    <Navbar/>
     <Home/>
-  )
+    <Footer/>
+    </>
+  );
 }
 
-export default App
+export default App;
