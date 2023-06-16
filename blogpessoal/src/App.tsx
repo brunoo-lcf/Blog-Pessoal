8cf00829955f914cd8f3e0cc66be955ba4f625de
Bruno Ferreira <brunoo.lcf10@gmail.com>
2023-06-16 19:28:33 -0300

    Adicionando tela de Login

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import Navbar from './components/static/navBar/Navbar';
+import Footer from './components/static/footer/Footer';
+import Home from './pages/home/Home';
 import './App.css';
-import Navbar from './components/static/navBar/Navbar.tsx';
-import Footer from './components/static/footer/Footer.tsx'
-import Home from './pages/home/Home.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+
 
 function App() {
-  return(
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: '65vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
