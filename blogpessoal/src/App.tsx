a27e228871723b96659b84b742f5cb25c2c32988
Bruno Ferreira <brunoo.lcf10@gmail.com>
2023-06-21 21:43:59 -0300

    Adicionando postagens

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -6,6 +6,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listatema/ListaTema';
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/postagem" element={<ListaPostagem />} />
         </Routes>
       </div>
       <Footer />
